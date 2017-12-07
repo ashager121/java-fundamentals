ad6dcff0302a491ecdd67f9ddb19d2cd7a2c8bae
Scott Hager <ASHAGER@Scotts-MacBook-Pro.local>
2017-12-07 16:29:41 -0500

    initial commit

---
+++ src/apples.java
@@ -0,0 +1,24 @@
+//switch basics
+import java.util.Scanner;
+
+class apples {
+    public static void main(String args[]) {
+    		int age;
+    		age = 3;
+    		
+    		switch (age) {
+    		case 1: 
+    			System.out.println("You can crawl");
+    			break;
+    		case 2:
+    			System.out.println("You can talk");
+    			break;
+    		case 3:
+			System.out.println("You can get in trouble");
+			break;
+    		}
+    }
+}
+
+//switches are used for referencing multiple statements
+//and returning if they are true or false
