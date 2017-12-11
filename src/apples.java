045c0e69ef50d42fae901595266d034b85d69a25
Scott Hager <ASHAGER@Scotts-MacBook-Pro.local>
2017-12-11 15:19:58 -0500

    updates

--- src/apples.java
+++ src/apples.java
@@ -3,9 +3,14 @@ import java.util.Scanner;
 
 class apples {
     public static void main(String args[]) {
-    	
-    	//calling upon everything within the tuna.java class file
-    	tuna tunaObject = new tuna();
-    	tunaObject.simpleMessage();
+    	//this functions as a call from tuna.java and implements a string with name
+    		Scanner input = new Scanner(System.in);
+    		tuna tunaObject = new tuna ();
+    		
+    		System.out.println("Enter your name here: ");
+    		String name = input.nextLine();
+    	//take the name argument and calls the String Name and prints out "Hello " + name	
+    		tunaObject.simpleMessage(name);
+    		
     }
 }
