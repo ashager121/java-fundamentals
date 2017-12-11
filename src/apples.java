99d449f809736b87bc8837d57d75a098abc3964e
Scott Hager <ASHAGER@Scotts-MacBook-Pro.local>
2017-12-11 10:07:36 -0500

    things I've done 12/11/17

--- src/apples.java
+++ src/apples.java
@@ -1,27 +1,11 @@
-//switch basics
+
 import java.util.Scanner;
 
 class apples {
     public static void main(String args[]) {
-    		int age;
-    		age = 3;
-    		
-    		switch (age) {
-    		case 1: 
-    			System.out.println("You can crawl");
-    			break;
-    		case 2:
-    			System.out.println("You can talk");
-    			break;
-    		case 3:
-			System.out.println("You can get in trouble");
-			break;
-		//if none of the cases are true, this is like an else statement	
-    		default: 
-    			System.out.println("I dont know how old you are");
-    			break;
+    	
+    	//calling upon everything within the tuna.java class file
+    	tuna tunaObject = new tuna();
+    	tunaObject.simpleMessage();
     }
 }
-
-//switches are used for referencing multiple statements
-//and returning if they are true or false
